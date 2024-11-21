e9b66ed8df102416f1743cd4c9dc48be944df8a0
Francisco <franjonas98@hotmail.com>
2024-11-21 20:54:54 -0300

    add class button

---
+++ GoCarGo/Button.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "src/base/Entity.h"
+
+class Entity;
+
+class Button : protected Entity
+{
+private:
+
+		Vector2 buttonPos;
+
+};
+
--- GoCarGo/src/base/Entity.h
+++ GoCarGo/src/base/Entity.h
@@ -4,8 +4,9 @@ class Entity
 {
 public:
 
+	virtual ~Entity();
+
 	virtual void Input() = 0;
 	virtual void Update() = 0;
 	virtual void Draw() = 0;
 };
-
