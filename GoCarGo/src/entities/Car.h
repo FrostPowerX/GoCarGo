31f73ec04fb386d0670309aed508553f1908d25a
Alex Gallardo <gallardonicolas.8100@outlook.com>
2024-11-21 22:48:25 -0300

    add car firm

---
+++ GoCarGo/src/entities/Car.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+#include "base/Entity.h"
+
+namespace game
+{
+	enum class Power{None};
+
+	class Car : public Entity
+	{
+	private:
+		float feul;
+		float distance;
+		Power activePower;
+
+		void Input() override;
+
+		void ChangeLane();
+
+		void CheckCollision();
+
+		void GetHit();
+
+		void AddFeul();
+
+	public:
+
+		Car(const Texture& texture, int width, int height, float feul);
+
+		void Input(int joystick = 0);
+		void Update() override;
+		void Draw() override;
+
+		GETTER_SETTER(Power, activePower);
+
+	};
+}
